dcbcc01f278e434c57f23c5aeaf2a0c04e4dbbec
Alessandro Sestini <alessandrosestini92@gmail.com>
2019-09-24 11:27:21 +0200

    Update export_graph.py

--- python-rl/export_graph.py
+++ python-rl/export_graph.py
@@ -45,34 +45,25 @@ def freeze_graph_mine(model_dir, output_node_names):
     if not output_node_names:
         print("You need to supply the name of a node to --output_node_names.")
         return -1
-
-    # We retrieve our checkpoint fullpath
+    
     checkpoint = tf.train.get_checkpoint_state(model_dir)
     input_checkpoint = checkpoint.model_checkpoint_path
 
-    # We precise the file fullname of our freezed graph
     absolute_model_dir = "/".join(input_checkpoint.split('/')[:-1])
     output_graph = absolute_model_dir + "/frozen_model.pb"
 
-    # We clear devices to allow TensorFlow to control on which device it will load operations
     clear_devices = True
 
-    # We start a session using a temporary fresh Graph
     with tf.Session(graph=tf.Graph()) as sess:
-        # We import the meta graph in the current default Graph
         saver = tf.train.import_meta_graph(input_checkpoint + '.meta', clear_devices=clear_devices)
 
-        # We restore the weights
         saver.restore(sess, input_checkpoint)
 
-        # We use a built-in TF helper to export variables to constants
         output_graph_def = tf.graph_util.convert_variables_to_constants(
-            sess,  # The session is used to retrieve the weights
-            tf.get_default_graph().as_graph_def(),  # The graph_def is used to retrieve the nodes
-            output_node_names.split(",")  # The output node names are used to select the usefull nodes
+            sess,
+            tf.get_default_graph().as_graph_def(),
+            output_node_names.split(",")
         )
-
-        # Finally we serialize and dump the output graph to the filesystem
         with tf.gfile.GFile(output_graph, "wb") as f:
             f.write(output_graph_def.SerializeToString())
         print("%d ops in the final graph." % len(output_graph_def.node))
